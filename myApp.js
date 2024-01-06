03a921c2a11ee5dbec85a5fbb28a33e9d569ca76
Martin Duvanel <martin.duvanel@a3.epfl.ch>
2024-01-06 20:41:51 +0100

    Add root response

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
 let express = require('express');
 let app = express();
 
-app.listen(port, function (req, res){
-    console.log("Hello World");
-})
+app.get('/', (req, res) => {
+  res.send("Hello Express");
+});
 
 
 
