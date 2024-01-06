629a8dbb821edff1e52e710f4fd9570071410bd2
Martin Duvanel <martin.duvanel@a3.epfl.ch>
2024-01-06 21:55:24 +0100

    Add body-parser

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
-require('dotenv').config()
+require('dotenv').config();
+let bodyParser = require('body-parser');
 
 let express = require('express');
 let app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip);
