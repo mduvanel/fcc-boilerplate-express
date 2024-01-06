a0720c5df167133f9b195cb445f020cc508ba55d
Martin Duvanel <martin.duvanel@a3.epfl.ch>
2024-01-06 22:01:06 +0100

    Add POST handler for /name

--- myApp.js
+++ myApp.js
@@ -37,8 +37,10 @@ app.get('/:word/echo', (req, res) => {
     res.json({echo: req.params.word});
 });
 
-app.get('/name', (req, res) => {
+app.route('/name').get((req, res) => {
     res.json({name: req.query.first + " " + req.query.last});
+}).post((req, res) => {
+    res.json({name: req.body.first + " " + req.body.last});
 });
 
 
