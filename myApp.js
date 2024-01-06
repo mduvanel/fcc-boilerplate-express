110963782c9b146e7fe1915777be255dd7e61f77
Martin Duvanel <martin.duvanel@a3.epfl.ch>
2024-01-06 20:53:44 +0100

    Serve CSS

--- myApp.js
+++ myApp.js
@@ -2,10 +2,10 @@ let express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-
+app.use('/public', express.static(__dirname + '/public'));
 
 
 
